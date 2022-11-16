ecf09b30e487ff442b651201939c29a735830261
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-11-16 18:13:45 -0300

    Add files via upload

---
+++ pousando na lua/sketch.js
@@ -0,0 +1,25 @@
+
+function preload()
+{
+
+}
+
+function setup() {
+  createCanvas(1000,700);
+  frameRate(80);
+
+  
+  rectMode(CENTER);
+  textSize(15);
+}
+
+function draw() 
+{
+  background(51);
+  
+
+  drawSprites();
+}
+
+
+
